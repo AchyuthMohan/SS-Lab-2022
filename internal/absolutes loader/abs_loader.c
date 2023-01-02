2ef553f09be21c96d9dcb85e2ce2cbbe8ff5ffa8
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-01-02 17:24:49 +0530

    self done absolutes loader

--- internal/absolutes loader/abs_loader.c
+++ internal/absolutes loader/abs_loader.c
@@ -1,46 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
 int main(){
     FILE *fp,*fp1;
-    char line[20],name[20],startaddr[20];
-
     fp=fopen("abssrc.txt","r");
-    fp1=fopen("Output.txt","w");
-    int i,j,sta;
-
-    int k=0;
+    fp1=fopen("outs.txt","w");
+    char name[20],line[30];
     fscanf(fp,"%s",line);
-    for(i=2;line[i]!='^';i++){
+    int k=0;
+    if(line[0]=='H'){
+       for(int i=2;line[i]!='^';i++){
         name[k++]=line[i];
+       }
     }
-    printf("Name of the program is : %s\n",name);
-
+    char startaddr[20];
+    int sta;
+    int i,j;
+    printf("The name of the program is %s",name);
+    fscanf(fp,"%s",line);
     while(!feof(fp)){
-        fscanf(fp,"%s",line);
-        while(line[0]=='T'){
-            for(i=2,j=0;line[i]!='^';i++,j++){
-                startaddr[j]=line[i];
-
-            }
-            sta=atoi(startaddr);
-            i=12;
-            while(line[i]!='$'){
-                if(line[i]!='^'){
-                    fprintf(fp1,"00%d\t%c\t%c\t\n",sta,line[i],line[i+1]);
-                    // printf("00%d\t%c\t%c\t\n",sta,line[i],line[i+1]);
-                    // printf("")
-                    i+=2;
-                    sta++;
-                }
-                else{
-                    i++;
-                }
-                
-            }
-            fscanf(fp,"%s",line);
+       while(line[0]=='T'){
+        for(i=2,j=0;line[i]!='^';i++,j++){
+            startaddr[j]=line[i];
         }
+        sta=atoi(startaddr);
+        i=12;
+        while(line[i]!='$'){
+            if(line[i]!='^'){
+                fprintf(fp1,"%d\t%c\t%c\n",sta,line[i],line[i+1]);
+                i=i+2;
+                sta++;
+            }
+            else{
+                i++;
+            }
+        }
+        fscanf(fp,"%s",line);
+       }
+    
     }
-
 }
