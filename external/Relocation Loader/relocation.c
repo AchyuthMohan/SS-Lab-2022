db9d4d7381384df3371a6bc222ce8467ab0af58d
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-11 19:37:56 +0530

    self relocation

--- external/Relocation Loader/relocation.c
+++ external/Relocation Loader/relocation.c
@@ -1,39 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 #define max 100
+int main(){
+    FILE *f1,*f2,*f3;
+    f1=fopen("input.txt","r");
+    f2=fopen("output.txt","w");
 
-int main()
-{
-    FILE *f1, *f2, *f3;
-    char line[max], addr[max];
-    int i, j;
-    int start_addr, hexaddr;
-    f1 = fopen("input.txt", "r");
-    f3 = fopen("output.txt", "w");
-    printf("Enter the actual start address: \n");
-    scanf("%x", &start_addr);
-    fscanf(f1, "%s", line);
-    while (!feof(f1))
-    {
-        if (line[0] == 'T')
-        {
-            for (i = 1, j = 0; i < 7; i++, j++)
-            {
-                addr[j] = line[i];
+    int start_addr;
+    printf("Enter the new starting address: \n");
+    scanf("%x",&start_addr);
+    char line[max];
+    char addr[max];
+    int k;
+    int hexaddr;
+    fscanf(f1,"%s",line);
+    while(!feof(f1)){
+
+        if(line[0]=='T'){
+            k=0;
+            for(int i=1;i<7;i++){
+                addr[k++]=line[i];
             }
-            f2 = fopen("temp.txt", "r+");
-            fprintf(f2, "%s", addr);
-            fscanf(f2, "%x", &hexaddr);
-            fclose(f2);
-            i = 9;
-            while (line[i] != '\0')
-            {
-                fprintf(f3, "%x\t%c%c\n", hexaddr + start_addr, line[i], line[i + 1]);
+            f3=fopen("temp.txt","r+");
+            fprintf(f3,"%s",addr);
+            fscanf(f3,"%x",&hexaddr);
+            k=9;
+            while(line[k]!='\0'){
+                fprintf(f2,"%x\t%c%c\n",hexaddr+start_addr,line[k],line[k+1]);
+                k+=2;
                 hexaddr++;
-                i += 2;
             }
         }
-        fscanf(f1, "%s", line);
+
+        fscanf(f1,"%s",line);
     }
 }
