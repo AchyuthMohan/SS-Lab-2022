e0cae2b08c6e0ea32b89443348b9bc816e1fa379
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-11 18:01:01 +0530

    relocation simplified

--- external/Relocation Loader/relocation.c
+++ external/Relocation Loader/relocation.c
@@ -1,118 +1,34 @@
-#include <stdio.h>
-#include <conio.h>
-#include <string.h>
-#include <stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define max 100
 
-void convert(char h[12]);
-char bitmask[12];
-char bit[12] = {0};
-int main()
-{
-    char add[6], length[10], input[10], binary[12], relocbit, ch, pn[5];
-    int start, inp, len, i, address, opcode, addr, actualadd, tlen;
-    FILE *fp1, *fp2;
-    printf("\n\n Enter the actual starting address : ");
-    scanf("%x", &start);
-    fp1 = fopen("input.txt", "r");
-    fp2 = fopen("output.txt", "w");
-    fscanf(fp1, "%s", input);
-    fprintf(fp2, " ----------------------------\n");
-    fprintf(fp2, " ADDRESS\tCONTENT\n");
-    fprintf(fp2, " ----------------------------\n");
-    while (strcmp(input, "E") != 0)
-    {
-        if (strcmp(input, "H") == 0)
-        {
-            fscanf(fp1, "%s", pn);   //program name
-            fscanf(fp1, "%x", &add);    //starting address
-            fscanf(fp1, "%x", &length);  //length of program
-            fscanf(fp1, "%s", input);   //next input
-        }
-        if (strcmp(input, "T") == 0)
-        {
-            fscanf(fp1, "%x", &address);
-            fscanf(fp1, "%x", &tlen);
-            fscanf(fp1, "%s", bitmask);
-            address += start;
-            convert(bitmask);
-            len = strlen(bit);
-            if (len >= 11)
-                len = 10;
-            for (i = 0; i < len; i++)
-            {
-                fscanf(fp1, "%x", &opcode);
-                fscanf(fp1, "%x", &addr);
-                relocbit = bit[i];
-                if (relocbit == '0')
-                    actualadd = addr;
-                else
-                    actualadd = addr + start;
-                fprintf(fp2, "\n  %x\t\t%x%x\n", address, opcode, actualadd);
-                address += 3;
+int main(){
+    FILE *f1,*f2,*f3;
+    char line[max],addr[max];
+    int i,j;
+    int start_addr,hexaddr;
+    f1=fopen("input.txt","r");
+    f3=fopen("output.txt","w");
+    printf("Enter the actual start address: \n");
+    scanf("%x",&start_addr);
+    do{
+        fscanf(f1,"%s",line);
+        if(line[0]=='T'){
+            for(i=1,j=0;i<7;i++,j++){
+                addr[j]=line[i];
             }
-            fscanf(fp1, "%s", input);
+            addr[j]='\0';
+            f2=fopen("temp.txt","r+");
+            fprintf(f2,"%s",addr);
+            fscanf(f2,"%x",&hexaddr);
+            fclose(f2);
+            i=9;
+            while(line[i]!='\0'){
+                fprintf(f3,"%x\t%c%c\n",hexaddr+start_addr,line[i],line[i+1]);
+                hexaddr++;
+                i+=2;
+            }
         }
-    }
-       
-}
-void convert(char h[12])
-{
-    int i, l;
-    strcpy(bit, "");
-    l = strlen(h);
-    for (i = 0; i < l; i++)
-    {
-        switch (h[i])
-        {
-        case '0':
-            strcat(bit, "0");
-            break;
-        case '1':
-            strcat(bit, "1");
-            break;
-        case '2':
-            strcat(bit, "10");
-            break;
-        case '3':
-            strcat(bit, "11");
-            break;
-        case '4':
-            strcat(bit, "100");
-            break;
-        case '5':
-            strcat(bit, "101");
-            break;
-        case '6':
-            strcat(bit, "110");
-            break;
-        case '7':
-            strcat(bit, "111");
-            break;
-        case '8':
-            strcat(bit, "1000");
-            break;
-        case '9':
-            strcat(bit, "1001");
-            break;
-        case 'A':
-            strcat(bit, "1010");
-            break;
-        case 'B':
-            strcat(bit, "1011");
-            break;
-        case 'C':
-            strcat(bit, "1100");
-            break;
-        case 'D':
-            strcat(bit, "1101");
-            break;
-        case 'E':
-            strcat(bit, "1110");
-            break;
-        case 'F':
-            strcat(bit, "1111");
-            break;
-        }
-    }
+    }while(!feof(f1));
 }
