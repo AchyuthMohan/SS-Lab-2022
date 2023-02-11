2f1c846ab91d047318d47381aab5481407d66b73
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-11 15:07:40 +0530

    self abs

--- external/absolute loader/abs_loader.c
+++ external/absolute loader/abs_loader.c
@@ -2,46 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 #define max 100
-
-FILE *f1, *f2;
-char line[max], name[max], start_addr[max];
-int i, start;
 int main()
 {
+    FILE *f1, *f2;
+    int k = 0;
+    char input[max], name[max];
     f1 = fopen("input.txt", "r");
     f2 = fopen("output.txt", "w");
-    fscanf(f1, "%s", line);
-    if (line[0] == 'H')
+    fscanf(f1, "%s", input);
+    if (input[0] == 'H')
     {
-        int k = 0;
-        for (i = 2; line[i] != '^'; i++)
+        for (int i = 2; input[i] != '^'; i++)
         {
-            name[k++] = line[i];
+            name[k++] = input[i];
         }
     }
-    fprintf(f2, "Name : %s\n", name);
-    fscanf(f1, "%s", line);
-    while (line[0] == 'T')
-    {   int v=0;
-        for(i=2;line[i]!='^';i++){
-            start_addr[v++]=line[i];
-        }
-        start=atoi(start_addr);
-        i=12;
-        while(line[i]!='$'){
-            
-            if(line[i]!='^'){
-                
-                fprintf(f2,"%d\t%c   %c\n",start,line[i],line[i+1]);
-                start++;
-                i+=2;
+    fprintf(f2, "Name: %s\n", name);
+    char start_addr[max];
+    int start;
+    fscanf(f1, "%s", input);
+    while (!feof(f1))
+    {
+        while (input[0] == 'T')
+        {
+            int v = 0;
+            for (int i = 2; input[i] != '^'; i++)
+            {
+                start_addr[v++] = input[i];
             }
-            else{
-                i++;
+            start = atoi(start_addr);
+            int c = 12;
+            while (input[c] != '$')
+            {
+                if (input[c] == '^')
+                {
+                    c++;
+                }
+                else
+                {
+                    fprintf(f2, "%d\t%c%c\n", start, input[c], input[c + 1]);
+                    c += 2;
+                    start++;
+                }
             }
+            fscanf(f1, "%s", input);
         }
-
-
-        fscanf(f1, "%s", line);
+        fscanf(f1, "%s", input);
     }
 }
