ceb14f0fd4bc5365869515d2be979a5ba70b1900
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-12 19:40:01 +0530

    best fit added

--- external/Memory allocations/Best Fit/best.c
+++ external/Memory allocations/Best Fit/best.c
@@ -1,35 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define max 100
+
 struct block
 {
-    int pno, bno, b_size, is_alloc;
+    int bno, pno, size, is_alloc;
 } blocks[max];
+
 int main()
 {
     int n, p_sizes[max], m;
-    printf("Enter the number of processes: ");
+    printf("Enter the number of proceses: \n");
     scanf("%d", &n);
-    printf("Enter the sizes of processes: \n");
+    printf("Enter the process sizes: \n");
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &p_sizes[i]);
     }
     printf("Enter the number of blocks: \n");
     scanf("%d", &m);
-    printf("Enter the sizes of blocks: \n");
-
+    printf("Enter the size of blocks: \n");
     for (int i = 0; i < m; i++)
     {
-        scanf("%d", &blocks[i].b_size);
         blocks[i].bno = i;
+        scanf("%d", &blocks[i].size);
         blocks[i].is_alloc = 0;
+    }
+    struct block temp;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = i + 1; j < m; j++)
+        {
+            if (blocks[j].size < blocks[i].size)
+            {
+                temp = blocks[i];
+                blocks[i] = blocks[j];
+                blocks[j] = temp;
+            }
+        }
     }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            if (blocks[j].is_alloc == 0 && p_sizes[i] <= blocks[j].b_size)
+            if (blocks[j].is_alloc == 0 && p_sizes[i]<=blocks[j].size)
             {
                 blocks[j].pno = i;
                 blocks[j].is_alloc = 1;
@@ -41,7 +55,6 @@ int main()
             }
         }
     }
-
     for (int i = 0; i < m; i++)
     {
         if (blocks[i].is_alloc != 0)
