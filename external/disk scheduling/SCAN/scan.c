9d19ea33dd7b31bfa879bce9fd601fdbedb648d1
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-13 13:25:44 +0530

    slef

--- external/disk scheduling/SCAN/scan.c
+++ external/disk scheduling/SCAN/scan.c
@@ -1,56 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define max 200
 int main(){
-    int n,size,initial,totlamoves=0,rq[100],i,j,index;
-    printf("Enter the number of requests: \n");
+    int n,rq[200],intial,totalMoves=0,i,j,temp,index,size;
+    printf("Enter the number of requests..\n");
     scanf("%d",&n);
     printf("Enter the request sequence: \n");
     for(i=0;i<n;i++){
         scanf("%d",&rq[i]);
     }
-    int temp;
+    printf("Enter the size of the disk: \n");
+    scanf("%d",&size);
+    printf("Enter the initial point header: \n");
+    scanf("%d",&intial);
     for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            temp=rq[i];
-            rq[i]=rq[j];
-            rq[j]=temp;
+        for(j=i+1;j,n;j++){
+            if(rq[i]>rq[j]){
+                temp=rq[i];
+                rq[i]=rq[j];
+                rq[j]=temp;
+            }
         }
     }
-    printf("Enter the initail position of head: \n");
-    scanf("%d",&initial);
     for(i=0;i<n;i++){
-        if(initial<=rq[i]){
+        if(intial<=rq[i]){
             index=i;
             break;
         }
     }
-    printf("1: to high 2: to low: \n");
+    printf("1: To high 2: To low\n");
     int move;
     scanf("%d",&move);
     if(move==1){
         for(i=index;i<n;i++){
-            totlamoves+=abs(rq[i]-initial);
-            initial=rq[i];
+            totalMoves+=abs(intial-rq[i]);
+            intial=rq[i];
         }
-        totlamoves+=abs(size-rq[i-1]-1);
-        totlamoves+=abs(size-1-0);
-        initial=0;
-        for(i=index-1;i>=0;i--){
-            totlamoves+=abs(rq[i]-initial);
-            initial=rq[i];
+        totalMoves+=abs(size-rq[i-1]-1);
+        totalMoves+=abs(size-1-0);
+        intial=0;
+        for(i=0;i<index;i++){
+            totalMoves+=abs(intial-rq[i]);
+            intial=rq[i];
         }
     }
     else{
         for(i=index-1;i>=0;i--){
-            totlamoves+=abs(rq[i]-initial);
-            initial=rq[i];
+            totalMoves+=abs(intial-rq[i]);
+            intial=rq[i];
         }
-        totlamoves+=abs(rq[i+1]-0);
-        totlamoves+=abs(size-1-0);
-        for(i=index-1;i>=0;i--){
-            totlamoves+=abs(rq[i]-initial);
-            initial=rq[i];
+        totalMoves+=abs(rq[i+1]-0);
+        totalMoves+=abs(size-1);
+        intial=size-1;
+        for(i=n-1;i>=index;i--){
+            totalMoves+=abs(intial-rq[i]);
+            intial=rq[i];
         }
     }
-    printf("Total moves: %d",totlamoves);
+    printf("Result: %d",totalMoves);
 }
