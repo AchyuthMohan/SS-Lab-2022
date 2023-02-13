6b3f1ee59bf76e9bb62e6fee682db0d9c6dc3881
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-13 13:05:14 +0530

    self cscan

--- external/disk scheduling/SCAN/scan.c
+++ external/disk scheduling/SCAN/scan.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define max 100
 int main(){
-    int rq[max],i,j,n,initial,totlaMovement=0,size,index,move;
-    printf("Enter the no of requests: \n");
+    int n,size,initial,totlamoves=0,rq[100],i,j,index;
+    printf("Enter the number of requests: \n");
     scanf("%d",&n);
     printf("Enter the request sequence: \n");
     for(i=0;i<n;i++){
         scanf("%d",&rq[i]);
     }
-    printf("Enter the size of disk: \n");
-    scanf("%d",&size);
     int temp;
     for(i=0;i<n;i++){
         for(j=i+1;j<n;j++){
@@ -19,7 +16,7 @@ int main(){
             rq[j]=temp;
         }
     }
-    printf("Enter the initial: \n");
+    printf("Enter the initail position of head: \n");
     scanf("%d",&initial);
     for(i=0;i<n;i++){
         if(initial<=rq[i]){
@@ -27,31 +24,33 @@ int main(){
             break;
         }
     }
-    printf("Direction 1:to high 2: to low: \n");
+    printf("1: to high 2: to low: \n");
+    int move;
     scanf("%d",&move);
     if(move==1){
         for(i=index;i<n;i++){
-            totlaMovement+=abs(rq[i]-initial);
+            totlamoves+=abs(rq[i]-initial);
             initial=rq[i];
         }
-        totlaMovement+=abs(rq[i-1]-1);
-        initial=size-1;
+        totlamoves+=abs(size-rq[i-1]-1);
+        totlamoves+=abs(size-1-0);
+        initial=0;
         for(i=index-1;i>=0;i--){
-            totlaMovement+=abs(rq[i]-initial);
+            totlamoves+=abs(rq[i]-initial);
             initial=rq[i];
         }
     }
     else{
         for(i=index-1;i>=0;i--){
-            totlaMovement+=abs(rq[i]-initial);
+            totlamoves+=abs(rq[i]-initial);
             initial=rq[i];
         }
-        totlaMovement+=abs(rq[i+1]-0);
-        initial=0;
-        for(i=index;i<n;i++){
-            totlaMovement+=abs(rq[i]-initial);
+        totlamoves+=abs(rq[i+1]-0);
+        totlamoves+=abs(size-1-0);
+        for(i=index-1;i>=0;i--){
+            totlamoves+=abs(rq[i]-initial);
             initial=rq[i];
         }
     }
-    printf("%d",totlaMovement);
+    printf("Total moves: %d",totlamoves);
 }
