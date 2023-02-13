c714b19ff3af869d3a76011235eef0c7f5443a29
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-13 12:20:55 +0530

    self scan

--- external/disk scheduling/SCAN/scan.c
+++ external/disk scheduling/SCAN/scan.c
@@ -1,74 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-    int RQ[100], totalheadmovement = 0, initial, n, move, size, i, j, temp;
-    printf("Enter the number of requests: ");
-    scanf("%d", &n);
-    printf("Enter the request queue: ");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &RQ[i]);
+#include<stdio.h>
+#include<stdlib.h>
+#define max 100
+int main(){
+    int rq[max],i,j,n,initial,totlaMovement=0,size,index,move;
+    printf("Enter the no of requests: \n");
+    scanf("%d",&n);
+    printf("Enter the request sequence: \n");
+    for(i=0;i<n;i++){
+        scanf("%d",&rq[i]);
     }
-
-    printf("Enter the initial position of head: ");
-    scanf("%d", &initial);
-    printf("Enter total disk size: ");
-    scanf("%d", &size);
-    printf("Enter the direction 1 for high and 0 for low: ");
-    scanf("%d", &move);
-
-    for (i = 0; i < n; i++)
-    {
-        for (j = i; j < n; j++)
-        {
-            if (RQ[j] < RQ[i])
-            {
-                temp = RQ[i];
-                RQ[i] = RQ[j];
-                RQ[j] = temp;
-            }
+    printf("Enter the size of disk: \n");
+    scanf("%d",&size);
+    int temp;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            temp=rq[i];
+            rq[i]=rq[j];
+            rq[j]=temp;
         }
     }
-    int index;
-    for (i = 0; i < n; i++)
-    {
-        if (initial < RQ[i])
-        {
-            index = i;
+    printf("Enter the initial: \n");
+    scanf("%d",&initial);
+    for(i=0;i<n;i++){
+        if(initial<=rq[i]){
+            index=i;
             break;
         }
     }
-    if (move == 1)
-    {
-        for (i = index; i < n; i++)
-        {
-            totalheadmovement = totalheadmovement + abs(RQ[i] - initial);
-            initial = RQ[i];
+    printf("Direction 1:to high 2: to low: \n");
+    scanf("%d",&move);
+    if(move==1){
+        for(i=index;i<n;i++){
+            totlaMovement+=abs(rq[i]-initial);
+            initial=rq[i];
         }
-        totalheadmovement = totalheadmovement + abs(RQ[i - 1] - 1);
-        initial = size - 1;
-        for (i = index - 1; i >= 0; i--)
-        {
-            totalheadmovement = totalheadmovement + abs(RQ[i] - initial);
-            initial = RQ[i];
-        }
-    }
-    else
-    {
-        for (i = index - 1; i >= 0; i--)
-        {
-            totalheadmovement = totalheadmovement + abs(RQ[i] - initial);
-            initial = RQ[i];
-        }
-        totalheadmovement = totalheadmovement + abs(RQ[i + 1] - 0);
-        initial = 0;
-        for (i = index; i < n; i++)
-        {
-            totalheadmovement = totalheadmovement + abs(RQ[i] - initial);
-            initial = RQ[i];
+        totlaMovement+=abs(rq[i-1]-1);
+        initial=size-1;
+        for(i=index-1;i>=0;i--){
+            totlaMovement+=abs(rq[i]-initial);
+            initial=rq[i];
         }
     }
-    printf("TOTAL HEAD MOVEMENT is: %d", totalheadmovement);
-    return 0;
+    else{
+        for(i=index-1;i>=0;i--){
+            totlaMovement+=abs(rq[i]-initial);
+            initial=rq[i];
+        }
+        totlaMovement+=abs(rq[i+1]-0);
+        initial=0;
+        for(i=index;i<n;i++){
+            totlaMovement+=abs(rq[i]-initial);
+            initial=rq[i];
+        }
+    }
+    printf("%d",totlaMovement);
 }
