9ae9f1c04755bb98f40bdc42bc85ead6e5933261
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-13 10:13:54 +0530

    linked modified

--- external/File Allocations/Linked/linked.c
+++ external/File Allocations/Linked/linked.c
@@ -1,55 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #define max 100
-
-int main()
-{
-    int f[max], n, bno, sta, length;
-    for (int i = 0; i < max; i++)
-    {
-        f[i] = 0;
+int main(){
+    int f[max],n,val,ch,b,start;
+    for(int i=0;i<max;i++){
+        f[i]=0;
     }
-    printf("Enter the number of blocks which are already allocated:\n");
-    scanf("%d", &n);
-    printf("Enter the blocks which are already allocated: \n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &bno);
-        f[bno] = 1;
+    printf("Enter the number of blocks which are already allocated: \n");
+    scanf("%d",&n);
+    printf("Enter the blocks which are aready allocated: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&val);
+        f[val]=1;
     }
-    a:printf("Enter the start address and length: \n");
-    scanf("%d", &sta);
-    scanf("%d", &length);
-    int k = length;
-    if (f[sta] == 0)
-    {
-        for (int i = sta; i < (sta + k); i++)
-        {
-            if(f[i]==0){
-                f[i]=1;
-                printf("%d------->%d\n",i,f[i]);
+    a:printf("1. add file 2. exit: \n");
+    scanf("%d",&ch);
+    switch(ch){
+        case 1:{
+            printf("Enter the no of blocks to be allocated and starting block: \n");
+            scanf("%d%d",&b,&start);
+            if(f[start]==1){
+                printf("Already alocated..");
+                goto a;
             }
-            else{
-                k++;
-                printf("already  allocated..\n");
+            for(int i=start;i<(start+b);i++){
+                if(f[i]==0){
+                    f[i]=1;
+                    printf("bno: %d\t---->%d\n",i,f[i]);
+                }
+                else{
+                    b++;
+                }
             }
+            goto a;
+            break;
+        }
+        case 2:{
+            exit(0);
         }
     }
-    else
-    {
-        printf("Starting address already allocated...\n");
-    }
-    b:printf("1. Add new file?  2.Exit: \n");
-    int ch;
-    scanf("%d",&ch);
-    if(ch==1){
-        goto a;
-    }
-    else if(ch==2){
-        exit(0);
-    }
-    else{
-        printf("Enter valid value..\n");
-        goto b;
-    }
+    
 }
